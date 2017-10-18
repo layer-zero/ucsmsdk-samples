8d10ac575b9628d284ab31a92a51550223df11c0
Tom Lijnse <tom.lijnse@layerzero.nl>
2017-10-18 15:45:44 +0200

    Renamed files

--- ucs-backup.py
+++ ucs-backup.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
-"""Import a UCS backup from file using the UCSM Python SDK"""
+"""Make a UCS backup using the UCSM Python SDK"""
 
 from ucsmsdk import ucshandle
-from ucsmsdk.utils.ucsbackup import import_ucs_backup
+from ucsmsdk.utils.ucsbackup import backup_ucs
 from getpass import getpass
 
 def main():
     ucsm_ip = raw_input('Target UCS IP address [https://192.168.218.182]: ') or '192.168.218.182'
     ucsm_user = raw_input('UCS user name [admin]: ') or 'admin'
     ucsm_password = getpass('UCS password: ')
-    filename = raw_input('Filename for backup (in current directory) [ucs_backup.xml]: ') or 'ucs_backup.xml'
+    filename = raw_input('Filename for backup (in current directory) [ucs-backup.xml]: ') or 'ucs-backup.xml'
     handle = ucshandle.UcsHandle(ucsm_ip, ucsm_user, ucsm_password, secure=True)
     handle.login()
-    import_ucs_backup(handle, file_dir="./", file_name=filename)
+    backup_ucs(handle, backup_type="config-all", file_dir="./", file_name=filename)
     handle.logout()
 
 if __name__ == '__main__':
